436c19cff1b248a9b96c5bc7d4cd18f3a50f525c
nikanorsky <kravtsovwindows@gmail.com>
2024-01-10 21:13:11 +0300

    feat: add exports

---
+++ server/controllers/index.js
@@ -0,0 +1,7 @@
+export * as UserController from './UserController.js';
+export * as BookController from './BookController.js';
+export * as ReviewController from './ReviewController.js';
+export * as FavoriteController from './FavoriteController.js';
+export * as FAQController from './FAQController.js';
+export * as NewsController from './NewsController.js';
+export * as FeedbackController from './FeedbackController.js'
