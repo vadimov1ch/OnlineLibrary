2dc5b5e14fa709f1dd5285f4f336e64ace16e2c4
nikanorsky <kravtsovwindows@gmail.com>
2024-01-10 21:07:24 +0300

    feat: add User model

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model('User', UserSchema);
