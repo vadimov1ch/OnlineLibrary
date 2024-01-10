6fdeb50d0218cda0c40c2d23e7ad8912dbe9b502
nikanorsky <kravtsovwindows@gmail.com>
2024-01-10 21:08:29 +0300

    feat: add Review model

---
+++ server/models/Review.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const ReviewSchema = new mongoose.Schema({
+    book: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+    },
+    comment: {
+        type: String,
+        required: true,
+    },
+    }, 
+    {
+        timestamps: true,
+    });
+
+export default mongoose.model('Review', ReviewSchema);
