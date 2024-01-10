5f3ca53d1f96018cd40646942dff6e883152683e
nikanorsky <kravtsovwindows@gmail.com>
2024-01-10 21:09:39 +0300

    feat: add News model

---
+++ server/models/News.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const NewsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+}},
+{
+  timestamps: true,
+});
+
+export default mongoose.model('News', NewsSchema);
