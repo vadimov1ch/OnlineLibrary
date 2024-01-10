01724ee2272d81c0e628397fe27948868d9371b4
nikanorsky <kravtsovwindows@gmail.com>
2024-01-10 21:17:08 +0300

    feat: create express app

--- server/index.js
+++ server/index.js
@@ -20,3 +20,7 @@ import {
 .connect('mongodb+srv://admin:admin123@devcluster.vmaf44v.mongodb.net/LibOnline?retryWrites=true&w=majority')
 .then(() => console.log('DataBase is OK'))
 .catch((err) => console.log('DataBase error', err));
+
+const app = express();
+app.use(express.json());
+app.use(cors()); 
