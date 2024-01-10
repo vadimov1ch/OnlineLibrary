3bcfc9a1167bd446eac40a32c206b722df06a6fc
nikanorsky <kravtsovwindows@gmail.com>
2024-01-10 21:15:27 +0300

    feat: add utils exports

---
+++ server/utils/index.js
@@ -0,0 +1,3 @@
+export { default as handleValidationErrors } from './handleValidationErrors.js';
+export { default as checkAuth } from './checkAuth.js';
+export { default as checkIsAdmin } from './checkIsAdmin.js';
